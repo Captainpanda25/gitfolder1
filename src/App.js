0acf77bed1090eabd3d6c74fc00eb3e6558c22e0
Captainpanda25 <adev7059@gmail.com>
2018-07-10 11:38:55 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -9,46 +9,9 @@ import { Widget, addResponseMessage, addLinkSnippet, addUserMessage } from 'reac
  
  
 class App extends React.Component {
-constructor(){
-  super();
-  this.state={};
 
-}
 
-  componentDidMount(){
-    var both="stud2";
-  const rootref = firebase.database().ref().child(both);
-  const nameref = rootref.child('Name');
-  const marref=rootref.child('marks');
-  const phoref=rootref.child('phone');
-  const regref=rootref.child('regid');
-  const semref=rootref.child('semester')
-  nameref.on('value',snap => {
-    this.setState({
-      Name: snap.val()
-    });
-  });
-    marref.on('value',snap => {
-    this.setState({
-      marks: snap.val()
-    });
-  });
-     phoref.on('value',snap => {
-    this.setState({
-      phone: snap.val()
-    });
-  });
-      regref.on('value',snap => {
-    this.setState({
-      regid: snap.val()
-    });
-  });
-       semref.on('value',snap => {
-    this.setState({
-      semester: snap.val()
-    });
-  });
-  }
+
   render() {
     return (
       <div>  
@@ -56,11 +19,6 @@ constructor(){
 	  <MSG/>
 	  <RSP/>
 	  <WBPG/>
-    <a>NAME: {this.state.Name}</a><div></div>
-    <a>MARKS: {this.state.marks}</a><div></div>
-    <a>Phone No: {this.state.phone}</a><div></div>
-    <a>REGISTER ID: {this.state.regid}</a><div></div>
-    <a>SEMESTER: {this.state.semester}</a>
       </div>
     );
   }
@@ -94,21 +52,68 @@ class MSG extends React.Component {
 }
 
 class RSP extends React.Component {
-  componentDidMount() {
-    addResponseMessage("How can I help you? Would you like to know your MARKS or EXAM DATES?");
+  constructor(){
+  super();
+  this.state={};
 
+}
+  componentDidMount() {
+    
+    addResponseMessage("How can I help you? Would you like to know your MARKS or EXAM DATES?");
+    var resp;
+
+    var both="stud1";
+  const rootref = firebase.database().ref().child(both);
+  const nameref = rootref.child('Name');
+  const marref=rootref.child('marks');
+  const phoref=rootref.child('phone');
+  const regref=rootref.child('regid');
+  const semref=rootref.child('semester')
+  
+  nameref.on('value',snap => {
+    this.setState({Name: snap.val()});
+  });
+    marref.on('value',snap => {
+    this.setState({marks: snap.val()});
+  });
+     phoref.on('value',snap => {
+    this.setState({phone: snap.val()});
+  });
+      regref.on('value',snap => {
+    this.setState({regid: snap.val()});
+  });
+       semref.on('value',snap => {
+    this.setState({semester: snap.val()});
+  });
+  
   }
  
   handleNewUserMessage = (response) => {
     console.log(`New message incoming! ${response}`);
     // Now send the message throught the backend API
-    addResponseMessage(response);                                      //COME BACK
+    if(response=="stud details")
+    {
+      addResponseMessage("Specify your name?");
+       
+   }
+    
+    if(response=="stud1"||response=="stud2")
+    {
+      //resp=response;
+     addResponseMessage("Details are as follows:");
+     addResponseMessage(this.state.Name);
+
+     } 
+    
+    else
+    addResponseMessage("HI");                                     //COME BACK
     
   }
 
   render() {
     return (
-      <div className="RSP">       
+      <div className="RSP"> 
+
         <Widget 
               handleNewUserMessage={this.handleNewUserMessage}
 		      profileAvatar={logo}
@@ -136,5 +141,9 @@ class WBPG extends React.Component {
     );
   }
 }
-
+/*          <a>NAME: {this.state.Name}</a><div></div>
+    <a>MARKS: {this.state.marks}</a><div></div>
+    <a>Phone No: {this.state.phone}</a><div></div>
+    <a>REGISTER ID: {this.state.regid}</a><div></div>
+    <a>SEMESTER: {this.state.semester}</a>      */
 export default App;
